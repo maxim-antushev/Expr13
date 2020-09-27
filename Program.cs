e37685c0e6db8b6cb4cfd04f133dfbb098fbfedd
maxim-antushev <71887070+maxim-antushev@users.noreply.github.com>
2020-09-27 15:39:32 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размер стороны забора");
+            double side = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите длину веревки");
+            double ropeLenght = int.Parse(Console.ReadLine());
+            double height = ropeLenght - side / 2;
+            double angle = 2 * Math.Acos(1 - height / ropeLenght);
+            if (side >= 2 * ropeLenght)
+                Console.WriteLine(ropeLenght * ropeLenght * Math.PI);
+            else
+                Console.WriteLine((ropeLenght * ropeLenght * Math.PI) - 4 * (ropeLenght * ropeLenght / 2 * (angle - Math.Sin(angle))));
+        }
+    }
+}
